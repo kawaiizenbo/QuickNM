7d3fdc01a719fd48765dfb84b850cc2440ab9171
kawaiizenbo <48113593+kawaiizenbo@users.noreply.github.com>
2022-07-26 08:27:52 -0700

    Add project files.

---
+++ QuickNM/MainForm.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace QuickNM
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.resolveHostnameCheckBox = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.endIPTextBox = new System.Windows.Forms.TextBox();
+            this.startIPTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.deviceListTreeView = new System.Windows.Forms.TreeView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.resolveHostnameCheckBox);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.endIPTextBox);
+            this.groupBox1.Controls.Add(this.startIPTextBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 96);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Scan Options";
+            // 
+            // resolveHostnameCheckBox
+            // 
+            this.resolveHostnameCheckBox.AutoSize = true;
+            this.resolveHostnameCheckBox.Checked = true;
+            this.resolveHostnameCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.resolveHostnameCheckBox.Location = new System.Drawing.Point(9, 73);
+            this.resolveHostnameCheckBox.Name = "resolveHostnameCheckBox";
+            this.resolveHostnameCheckBox.Size = new System.Drawing.Size(121, 17);
+            this.resolveHostnameCheckBox.TabIndex = 4;
+            this.resolveHostnameCheckBox.Text = "Resolve Hostnames";
+            this.resolveHostnameCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "End IP Address";
+            // 
+            // endIPTextBox
+            // 
+            this.endIPTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.endIPTextBox.Location = new System.Drawing.Point(150, 45);
+            this.endIPTextBox.Name = "endIPTextBox";
+            this.endIPTextBox.Size = new System.Drawing.Size(204, 20);
+            this.endIPTextBox.TabIndex = 2;
+            this.endIPTextBox.Text = "192.168.0.255";
+            // 
+            // startIPTextBox
+            // 
+            this.startIPTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startIPTextBox.Location = new System.Drawing.Point(150, 19);
+            this.startIPTextBox.Name = "startIPTextBox";
+            this.startIPTextBox.Size = new System.Drawing.Size(204, 20);
+            this.startIPTextBox.TabIndex = 1;
+            this.startIPTextBox.Text = "192.168.0.0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start IP Address";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.saveButton);
+            this.groupBox2.Controls.Add(this.startButton);
+            this.groupBox2.Location = new System.Drawing.Point(12, 114);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(360, 60);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Transport Controls";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(112, 19);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(100, 35);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(6, 19);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(100, 35);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.deviceListTreeView);
+            this.groupBox3.Location = new System.Drawing.Point(12, 180);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(360, 356);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Device List";
+            // 
+            // deviceListTreeView
+            // 
+            this.deviceListTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.deviceListTreeView.Location = new System.Drawing.Point(9, 19);
+            this.deviceListTreeView.Name = "deviceListTreeView";
+            this.deviceListTreeView.Size = new System.Drawing.Size(345, 331);
+            this.deviceListTreeView.TabIndex = 0;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(384, 22);
+            this.statusStrip1.TabIndex = 3;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(33, 17);
+            this.statusLabel.Text = "Init...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MainForm";
+            this.Text = "QuickNM";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox resolveHostnameCheckBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox endIPTextBox;
+        private System.Windows.Forms.TextBox startIPTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TreeView deviceListTreeView;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+    }
+}
+
---
+++ QuickNM/MainForm.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace QuickNM
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("iphlpapi.dll", ExactSpelling = true)]
+        public static extern int SendARP(int DestIP, int SrcIP, [Out] byte[] pMacAddr, ref int PhyAddrLen);
+        private static Dictionary<string, string> oui = new Dictionary<string, string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // load oui db
+            string[] ouiRawText = Resources.oui.Split('\n');
+            if (File.Exists("oui.ini"))
+            {
+                ouiRawText = File.ReadAllLines("oui.ini");
+            }
+            foreach (string s in ouiRawText)
+            {
+                if (s.Trim() == "") { continue; }
+                oui.Add(s.Split('=')[0].Trim(), s.Split('=')[1].Split('\\')[0].Trim());
+            }
+            statusLabel.Text = "Ready.";
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            // Check if IPs are in valid format
+            if (!(VerifyIPv4(startIPTextBox.Text) && VerifyIPv4(endIPTextBox.Text)))
+            {
+                MessageBox.Show("IP is invalid.", "Error");
+                return;
+            }
+
+            // clear treeview
+            deviceListTreeView.Nodes.Clear();
+
+            // Convert IPs to byte[]
+            byte[] startIPAddress = IPv4String2ByteArray(startIPTextBox.Text);
+            byte[] endIPAddress = IPv4String2ByteArray(endIPTextBox.Text);
+
+            Ping ping = new Ping();
+
+            // god have mercy on us
+            for (int i = startIPAddress[0]; i <= endIPAddress[0]; i++)
+            {
+                for (int j = startIPAddress[1]; j <= endIPAddress[1]; j++)
+                {
+                    for (int k = startIPAddress[2]; k <= endIPAddress[2]; k++)
+                    {
+                        for (int l = startIPAddress[3]; l <= endIPAddress[3]; l++)
+                        {
+                            statusLabel.Text = $"Pinging: {i}.{j}.{k}.{l}";
+                            try
+                            {
+                                PingReply reply = ping.Send($"{i}.{j}.{k}.{l}", 1);
+                                if (reply.Address != null)
+                                {
+                                    deviceListTreeView.Nodes.Add(reply.Address.ToString());
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine(ex.Message);
+                            }
+                            Application.DoEvents();
+                        }
+                    }
+                }
+            }
+
+            // get mac addresses with dark witchcraft (System.Runtime.InteropServices)
+            statusLabel.Text = "Searching MAC Addresses.";
+            int m = 0;
+            foreach (var device in deviceListTreeView.Nodes)
+            {
+                try
+                {
+                    IPAddress hostIPAddress = IPAddress.Parse(device.ToString().Replace("TreeNode: ", ""));
+                    byte[] ab = new byte[6];
+                    int len = ab.Length,
+                        r = SendARP((int)hostIPAddress.Address, 0, ab, ref len);
+                    string ouiString = oui[BitConverter.ToString(ab, 0, 3).Replace("-", "")];
+                    deviceListTreeView.Nodes[m].Nodes.Add($"MAC Address: {BitConverter.ToString(ab, 0, 6)} ({ouiString})");
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                m++;
+                Application.DoEvents();
+            }
+
+            // get hostnames if wanted
+            if (resolveHostnameCheckBox.Checked)
+            {
+                Application.DoEvents();
+                statusLabel.Text = "Resolving hostnames.";
+                int n = 0;
+                foreach (var device in deviceListTreeView.Nodes)
+                {
+                    try
+                    {
+                        string hostname = "[None]";
+                        IPHostEntry entry = Dns.GetHostEntry(device.ToString().Replace("TreeNode: ", ""));
+                        if (entry != null)
+                        {
+                            hostname = entry.HostName;
+                        }
+                        deviceListTreeView.Nodes[n].Nodes.Add($"Hostname: {hostname}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    n++;
+                    Application.DoEvents();
+                }
+            }
+            statusLabel.Text = "Done.";
+        }
+
+        public bool VerifyIPv4(string ip)
+        {
+            if (ip == null) { return false; }
+            if (ip.Trim().Length == 0) { return false; }
+            string[] parts = ip.Split('.');
+            if (parts.Length != 4) { return false; }
+            foreach (string part in parts)
+            {
+                if (part.Trim().Length == 0) { return false; }
+                try { if (int.Parse(part) > 255) { return false; } }
+                catch { return false; }
+            }
+            return true;
+        }
+
+        public byte[] IPv4String2ByteArray(string ip)
+        {
+            byte[] bytes = {
+                byte.Parse(ip.Split('.')[0]),
+                byte.Parse(ip.Split('.')[1]),
+                byte.Parse(ip.Split('.')[2]),
+                byte.Parse(ip.Split('.')[3]),
+            };
+            return bytes;
+        }
+    }
+}
